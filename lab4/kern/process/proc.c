e48119a98c3005077675a42084ea7352c4545767
PAisHere <162877565+PAisHere@users.noreply.github.com>
2024-12-07 01:29:44 +0800

    proc.c

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -285,31 +285,39 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
-    /*
-     * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
-     * MACROs or Functions:
-     *   alloc_proc:   create a proc struct and init fields (lab4:exercise1)
-     *   setup_kstack: alloc pages with size KSTACKPAGE as process kernel stack
-     *   copy_mm:      process "proc" duplicate OR share process "current"'s mm according clone_flags
-     *                 if clone_flags & CLONE_VM, then "share" ; else "duplicate"
-     *   copy_thread:  setup the trapframe on the  process's kernel stack top and
-     *                 setup the kernel entry point and stack of process
-     *   hash_proc:    add proc into proc hash_list
-     *   get_pid:      alloc a unique pid for process
-     *   wakeup_proc:  set proc->state = PROC_RUNNABLE
-     * VARIABLES:
-     *   proc_list:    the process set's list
-     *   nr_process:   the number of process set
-     */
+    // 1. 调用 alloc_proc 分配一个新的进程结构
+    proc = alloc_proc();
+    if (proc == NULL) {
+        goto bad_fork_cleanup_proc;
+    }
+    proc->parent = current;
+    // 2. 调用 setup_kstack 为子进程分配内核栈
+    if (setup_kstack(proc) != 0) {
+        goto bad_fork_cleanup_proc;
+    }
 
-    //    1. call alloc_proc to allocate a proc_struct
-    //    2. call setup_kstack to allocate a kernel stack for child process
-    //    3. call copy_mm to dup OR share mm according clone_flag
-    //    4. call copy_thread to setup tf & context in proc_struct
-    //    5. insert proc_struct into hash_list && proc_list
-    //    6. call wakeup_proc to make the new child process RUNNABLE
-    //    7. set ret vaule using child proc's pid
+    // 3. 判断是否需要复制内存管理信息
+    
+        
+    if (copy_mm(clone_flags, proc) != 0) {
+        goto bad_fork_cleanup_kstack;
+    }
+    
+
+    // 4. 调用 copy_thread 设置 tf 和 context
+    copy_thread(proc, stack, tf);
+    
+    // 5. 将新进程加入哈希链和进程列表
+    
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &proc->list_link);
+    nr_process++;
+    // 6. 调用 wakeup_proc 将新进程置为可调度
+    wakeup_proc(proc);
+
+    // 7. 设置返回值为子进程的 PID
+    ret = proc->pid;
 
     
 
